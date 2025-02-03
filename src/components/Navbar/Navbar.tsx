c6f795c1f9a7e9f281e185c9713ff1b33a97fbc0
Biki Balami <bikibalami99@gmail.com>
2025-02-03 20:47:21 +0900

    implement styles for navBar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -2,32 +2,58 @@ import Link from "next/link";
 import styles from "./Navbar.module.css";
 import { getServerSession } from "next-auth";
 import { authOptions } from "@/lib/auth";
+import Image from "next/image";
 
 export default async function Navbar() {
     const session = await getServerSession(authOptions);
     return (
         <nav className={styles.navBar}>
             <Link href="/" className={styles.logo}>
-                Twitter
+                <h1 className={styles.heading}>Pickiku Pachiku</h1>
             </Link>
             <ul className={styles.navLinks}>
                 <li>
-                    <Link href="/">Home</Link>
+                    <Link className={styles.icon} href="/">
+                        <Image
+                            src="/icons/home-icon.svg"
+                            width={20}
+                            height={26}
+                            alt="Home Icon"
+                        />
+                    </Link>
                 </li>
                 <li>
-                    {session && (
-                        <Link href="/api/auth/signout">
-                            <button>Sign out</button>
-                        </Link>
-                    )}
+                    <Link className={styles.icon} href="/dashboard">
+                        <Image
+                            src="/icons/profile-icon.svg"
+                            width={14}
+                            height={25}
+                            alt="Home Icon"
+                        />
+                    </Link>
                 </li>
-                <li>
-                    {!session && (
-                        <Link href="/api/auth/signin">
-                            <button>Sign in</button>
+
+                {session && (
+                    <li>
+                        <Link
+                            className="button secondaryButton"
+                            href="/api/auth/signout"
+                        >
+                            Sign out
                         </Link>
-                    )}
-                </li>
+                    </li>
+                )}
+
+                {!session && (
+                    <li>
+                        <Link
+                            className="button secondaryButton"
+                            href="/api/auth/signin"
+                        >
+                            Sign in
+                        </Link>
+                    </li>
+                )}
             </ul>
         </nav>
     );
