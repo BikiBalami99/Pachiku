3801aaedb85ef0f22241227cd72d2bf25b1a1d26
Biki Balami <bikibalami99@gmail.com>
2025-02-04 20:58:56 +0900

    Bug fix in HomePage with unused session

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,5 @@
 import Feed from "@/components/Feed/Feed";
-import { getServerSession } from "next-auth";
-
 export default async function Home() {
-    const session = await getServerSession();
     return (
         <div>
             <Feed />
