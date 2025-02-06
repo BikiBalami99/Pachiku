7e2d8729c83f81648789978c039302d626bee243
Biki Balami <bikibalami99@gmail.com>
2025-02-07 00:23:33 +0900

    Bug fix of laggy heart action + UI reflecting heart action loading phase

--- src/components/APachikuComponents/LikeCommentShareComponents/LikeCommentShareComponents.tsx
+++ src/components/APachikuComponents/LikeCommentShareComponents/LikeCommentShareComponents.tsx
@@ -20,6 +20,7 @@ export function HeartIcon({
 
     // Handle click with debounce and error handling
     const handleClick = async () => {
+        console.log("clicked");
         if (isSubmitting) return; // Prevent double-taps
         setIsSubmitting(true);
 
@@ -55,22 +56,23 @@ export function HeartIcon({
     };
 
     // Dynamic heart icon class
-    const heartClassName = `${styles.heartIcon} ${
-        heartClicked ? styles.heartIconClicked : ""
-    }`;
+    const heartClassName = `${styles.heartIcon} 
+    ${heartClicked ? styles.heartIconClicked : ""} 
+    ${isSubmitting ? styles.heartIconDeactivated : ""}`;
 
     return (
         <div className={styles.likeContainer}>
-            <button
+            <svg
+                width="25"
+                height="23"
+                viewBox="0 0 25 23"
                 className={heartClassName}
                 onClick={handleClick}
-                disabled={isSubmitting}
                 aria-label={heartClicked ? "Unlike post" : "Like post"}
             >
-                <svg width="25" height="23" viewBox="0 0 25 23">
-                    <path d="M12.5 4C11.5 2 9.18367 0.992195 7.47013 1.00007C5.75659 1.00794 4.11548 1.69214 2.90381 2.90381C1.69214 4.11548 1.00794 5.75659 1.00007 7.47013C0.992195 9.18368 1.66128 10.831 2.86176 12.0538L3.21576 12.4078L12.4078 21.5998L21.5998 12.4078L21.9538 12.0538C23.1447 10.8291 23.8057 9.18484 23.7937 7.47662C23.7818 5.76841 23.0978 4.13357 21.8898 2.92571C20.6818 1.71786 19.0469 1.03409 17.3387 1.02231C15.6305 1.01053 13.5 2 12.5 4Z" />
-                </svg>
-            </button>
+                <path d="M12.5 4C11.5 2 9.18367 0.992195 7.47013 1.00007C5.75659 1.00794 4.11548 1.69214 2.90381 2.90381C1.69214 4.11548 1.00794 5.75659 1.00007 7.47013C0.992195 9.18368 1.66128 10.831 2.86176 12.0538L3.21576 12.4078L12.4078 21.5998L21.5998 12.4078L21.9538 12.0538C23.1447 10.8291 23.8057 9.18484 23.7937 7.47662C23.7818 5.76841 23.0978 4.13357 21.8898 2.92571C20.6818 1.71786 19.0469 1.03409 17.3387 1.02231C15.6305 1.01053 13.5 2 12.5 4Z" />
+            </svg>
+
             <span className={styles.likeCount}>{numOfLikes}</span>
         </div>
     );
