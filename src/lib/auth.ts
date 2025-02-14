e501c9f19af1bcf75dd39448517ab97a99f52d84
Biki Balami <bikibalami99@gmail.com>
2025-02-14 14:22:23 +0900

    Bug fix with auth.ts

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -4,13 +4,18 @@ import GoogleProvider, { type GoogleProfile } from "next-auth/providers/google";
 import { prisma } from "@/lib/prisma";
 import { User } from "@prisma/client";
 
-const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID!;
-const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET!;
+const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
+const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
+
+if (!GOOGLE_CLIENT_ID || !GOOGLE_CLIENT_SECRET) {
+    throw new Error("Missing Google OAuth environment variables");
+}
 
 export const authOptions: NextAuthOptions = {
     session: {
         strategy: "jwt",
     },
+    secret: process.env.NEXTAUTH_SECRET,
     providers: [
         GoogleProvider({
             clientId: GOOGLE_CLIENT_ID,
@@ -26,14 +31,11 @@ export const authOptions: NextAuthOptions = {
 
             const { email, given_name, family_name, picture } =
                 profile as GoogleProfile;
-
             const defaultUserName = `user-${randomUUID()}`;
             const image = picture ?? "/icons/no-image-icon.svg";
 
             const upsertedUser = await prisma.user.upsert({
-                where: {
-                    email: profile.email,
-                },
+                where: { email },
                 create: {
                     email,
                     firstName: given_name,
@@ -41,28 +43,20 @@ export const authOptions: NextAuthOptions = {
                     username: defaultUserName,
                     image,
                 },
-                update: {
-                    firstName: given_name,
-                    lastName: family_name,
-                },
+                update: { firstName: given_name, lastName: family_name },
             });
 
             console.log("Upserted User: ", upsertedUser);
 
             return true;
         },
-        async jwt({ token }) {
-            // Fetch user from DB on every JWT update
-            const dbUser = await prisma.user.findUnique({
-                where: { email: token.email! },
-            });
-            if (dbUser) {
-                token.user = dbUser;
+        async jwt({ token, user }) {
+            if (user) {
+                token.user = user;
             }
             return token;
         },
         async session({ session, token }) {
-            // Assign database user to session
             if (token.user) {
                 session.user = token.user as User;
             }
